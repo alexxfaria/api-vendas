bb549d7bbe194de57aa206d05ad2a58552b97b1d
alexxfaria <alexxfs@live.com>
2022-01-12 16:51:30 -0300

    add auth

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/repositories/UsersRepositories';
@@ -27,9 +28,9 @@ class CreateSessionService {
     if (!passwordConfirmed) {
       throw new AppError('incorrect email/password combination', 401);
     }
-    const token = sign({}, 'minhasenhasecreta', {
+    const token = sign({}, authConfig.jwt.secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: authConfig.jwt.expiresIn,
     });
 
     return { user, token };
