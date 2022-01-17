f1e4518858e7f6dbe9ba419df408f1a788f0aff5
alexxfaria <alexxfs@live.com>
2022-01-17 11:16:10 -0300

    add service delete e update customers

--- src/modules/customers/controllers/CustomersController.ts
+++ src/modules/customers/controllers/CustomersController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import CreateCustomerService from '../services/CreateCustomerService';
+import DeleteCustomerService from '../services/DeleteCustomerService';
 import ListCustomerService from '../services/ListCustomerService';
 import ShowCustomerService from '../services/ShowCustomerService';
+import UpdateCustomerService from '../services/UpdateCustomerService';
 
 class CustomersController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -28,6 +30,25 @@ class CustomersController {
     const customers = await listCustomers.execute();
 
     return res.json(customers);
+  }
+  public async update(req: Request, res: Response): Promise<Response> {
+    const { name, email } = req.body;
+    const { id } = req.params;
+
+    const updateCustomer = new UpdateCustomerService();
+
+    const customer = await updateCustomer.execute({ id, name, email });
+
+    return res.json(customer);
+  }
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const deleteCustomer = new DeleteCustomerService();
+
+    await deleteCustomer.execute({ id });
+
+    return res.json([]);
   }
 }
 export default CustomersController;
--- src/modules/customers/routes/customers.routes.ts
+++ src/modules/customers/routes/customers.routes.ts
@@ -28,5 +28,23 @@ customersRouter.get(
   }),
   customersController.show,
 );
+customersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+  }),
+  customersController.update,
+);
+customersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+  }),
+  customersController.delete,
+);
 
 export default customersRouter;
