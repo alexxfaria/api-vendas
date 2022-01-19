1f0967cdd5ebfb5dcdaf78f04c943673ead626de
alexxfaria <alexxfs@live.com>
2022-01-19 15:02:00 -0300

    Add entiti order

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,26 @@
+import Customers from '@modules/customers/typeorm/entities/Customers';
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('orders')
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Customers)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customers;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+export default Order;
--- src/shared/typeorm/migrations/1642613815336-AddProductIdToOrdersProducts.ts
+++ src/shared/typeorm/migrations/1642613815336-AddProductIdToOrdersProducts.ts
@@ -1,21 +1,21 @@
 import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from 'typeorm';
 
-export class AddCustomerIdOrders1642610947581 implements MigrationInterface {
+export class AddProductIdToOrdersProducts1642613815336 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
-      'orders',
+      'orders_products',
       new TableColumn({
-        name: 'customer_id',
+        name: 'product_id',
         type: 'uuid',
         isNullable: true,
       }),
     );
     await queryRunner.createForeignKey(
-      'orders',
+      'orders_products',
       new TableForeignKey({
-        name: 'OrdersCustomer',
-        columnNames: ['customer_id'],
-        referencedTableName: 'customers',
+        name: 'OrdersProductsProduct',
+        columnNames: ['product_id'],
+        referencedTableName: 'products',
         referencedColumnNames: ['id'],
         onDelete: 'SET NULL',
       }),
@@ -23,7 +23,7 @@ export class AddCustomerIdOrders1642610947581 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey('orders', 'OrdersCustomer');
-    await queryRunner.dropColumn('orders', 'customer_id');
+    await queryRunner.dropForeignKey('orders_products', 'OrdersProductsProduct');
+    await queryRunner.dropColumn('orders_products', 'product_id');
   }
 }
