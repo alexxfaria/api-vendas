b01317eb4a326d797809a1b2cdae3fd08a03a56d
alexxfaria <alexxfs@live.com>
2022-01-19 17:06:27 -0300

    create order service pedido de venda

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -1,7 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
-import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
 
 interface IRequest {
   id: string;
@@ -11,12 +11,7 @@ interface IRequest {
 }
 
 class UpdateProductService {
-  public async execute({
-    id,
-    name,
-    price,
-    quantity,
-  }: IRequest): Promise<Product> {
+  public async execute({ id, name, price, quantity }: IRequest): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
     const product = await productsRepository.findOne(id);
     if (!product) {
